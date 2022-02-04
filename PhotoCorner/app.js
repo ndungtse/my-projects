2b462dee6e48697cd15a685e6351fe41af75828a
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-02-04 18:17:49 +0200

    main

--- PhotoCorner/app.js
+++ PhotoCorner/app.js
@@ -1,14 +1,49 @@
 
 let settings = document.querySelector('.settings')
+let message = document.querySelector('.message')
+let flex = document.querySelector('#flex')
  function setFlex(){
-     if(settings.style.display==='none'){
-    settings.style.display = 'flex' 
+     settings.classList.toggle('set-active')
 }
-else{
-    settings.style.display = 'none' 
-}
-}
+flex.addEventListener('click', setFlex)
 
 function closeFrame(){
-    settings.style.display = 'none' 
+    settings.classList.remove('set-active')
 }
+
+function showMess() {
+    if(message.style.display==='none'){
+        message.style.display = 'flex' 
+    }
+    else{
+        message.style.display = 'none' 
+    }
+}
+function closeAll() {
+    message.style.display = 'none' 
+    settings.style.display = 'none'
+}
+
+let theicon = document.querySelector('#toggle-left')
+let body = document.body;
+function theme() {
+      body.classList.toggle('dark-theme');
+      if(body.classList.contains('dark-theme')){
+           theicon.src="Images/toggle-right.png";
+      }
+      else{
+          theicon.src="Images/toggle-left.png";
+      }
+}
+/* 
+var icon = document.getElementById("icon")
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src="Images/sun.png";
+    }
+    else{
+        icon.src= "Images/moon.png";
+    }
+} */
